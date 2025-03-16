c01d2fc77376d8798a66e7f61befbd5cb70d0105
whitegrimreaper <theDumb12@yahoo.com>
2025-03-16 03:18:11 -0700

    Game list pull working

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"context"
+	//"context"
 	"fmt"
+	"os"
 	
 
 	"github.com/joho/godotenv"
 	"github.com/Jleagle/steam-go/steamapi"
 )
+
+type Game struct {
+	ID       uint   `gorm:"primaryKey"`
+	AppID    uint64 `gorm:"uniqueIndex"`
+	Name     string
+	Playtime uint64
+}
 
 func init() {
 	if err := godotenv.Load(); err != nil {
@@ -17,4 +25,15 @@ func init() {
 
 func main() {
 	apiKey := os.Getenv("STEAM_API_KEY")
+	//ctx :=  context.Background();
+	steamID := 76561198057886745
+
+	client := steamapi.NewClient()
+	client.SetKey(apiKey)
+	games, err := client.GetOwnedGames(int64(steamID))
+	if err != nil {
+		return
+	}
+
+	fmt.Printf("Retrieved %d games\n", games.GameCount)
 }
