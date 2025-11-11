694f25480b13a13d0d708afa28b1fbdd6c07cdb2
whitegrimreaper <theDumb12@yahoo.com>
2025-11-10 19:02:55 -0800

    Added skeleton for expensive all NSFW call, fixed a log line

---
+++ helpers.go
@@ -0,0 +1,7 @@
+package main
+
+func shouldCheckGame(game GameEntry)(resp bool) {
+	// resp = false; <-- add this as default
+	// TODO: Implement this
+	return false;
+}
--- main.go
+++ main.go
@@ -18,8 +18,9 @@ import (
 var SteamDb *gorm.DB
 
 func init() {
+	// env handling
 	if err := godotenv.Load(); err != nil {
-		fmt.Printf("No .env file found")
+		fmt.Printf("No .env file found\n")
 	}
 }
 
@@ -30,6 +31,7 @@ func main() {
 
 	SteamDb = setupDB();
 
+	// init client and get game list (one call)
 	client := steamapi.NewClient()
 	client.SetKey(apiKey)
 	gamesResp, err := client.GetOwnedGames(int64(steamID))
@@ -61,7 +63,7 @@ func main() {
 	}
 }
 
-func checkGamesForNSFWContent(client steamapi.Client) {
+func checkGamesForNSFWContent(client steamapi.Client, checkAllGames bool) {
 	// This is a very expensive function to call, and will make an API call for every item in your steam library.
 	// For me that is 1k plus calls per run, more than 1% of the 100k daily call limit
 	// Use sparingly
@@ -69,28 +71,33 @@ func checkGamesForNSFWContent(client steamapi.Client) {
 	allGames := getAllGameEntries()
 
 	for _, game := range allGames {
-		// This is the issue line
-		details, err := client.GetAppDetails(uint(game.AppID), COUNTRY_CODE, LANGUAGE_CODE, []string{})
-		if err != nil {
-			// Keep randomly getting errors here for specific games
-			// Some are removed like MapleStory 2, some are private/dupe style games like many test servers
-			fmt.Printf("Issue getting info for app: %d, name %s\n", game.AppID, game.Name)
-			fmt.Printf("Error: %v\n", err.Error())
-			fmt.Printf("Could be getting rate limited, or the game is removed")
-		} else if ids, ok := details.Data.ContentDescriptors.IDs.([]interface{}); ok {
-			for _, v := range ids {
-				//fmt.Printf("%v\n", reflect.TypeOf(v))
-				// So the content descriptors are for violence and sexual content
-				// 2 is for violence and gore, which is fine on Twitch/YT in games
-				// 5 is for any mature content, also not very helpful (this includes violence)
-				// 1 is general nudity or sexual content, 3 is for "adult only" sexual content, and 4 is "gratuitous" sexual content.
-				// Personally I don't have many nsfw games, but I would generally say that 3 and 4 are tags for games you should not play on
-				// regular livestreams. 1 is hit or miss
-				if num, ok := v.(float64); ok && (num == 3 || num == 4){
-					fmt.Printf("This game likely has sexual content and shouldn't be included: %s\n", game.Name)
-					tagGameAsNSFW(game.AppID, true)
+		if(checkAllGames || shouldCheckGame(game)) {
+			// This is the issue line
+			details, err := client.GetAppDetails(uint(game.AppID), COUNTRY_CODE, LANGUAGE_CODE, []string{})
+			if err != nil {
+				// Keep randomly getting errors here for specific games
+				// Some are removed like MapleStory 2, some are private/dupe style games like many test servers
+				fmt.Printf("Issue getting info for app: %d, name %s\n", game.AppID, game.Name)
+				fmt.Printf("Error: %v\n", err.Error())
+				fmt.Printf("Could be getting rate limited, or the game is removed")
+			} else if ids, ok := details.Data.ContentDescriptors.IDs.([]interface{}); ok {
+				for _, v := range ids {
+					//fmt.Printf("%v\n", reflect.TypeOf(v))
+					// So the content descriptors are for violence and sexual content
+					// 2 is for violence and gore, which is fine on Twitch/YT in games
+					// 5 is for any mature content, also not very helpful (this includes violence)
+					// 1 is general nudity or sexual content, 3 is for "adult only" sexual content, and 4 is "gratuitous" sexual content.
+					// Personally I don't have many nsfw games, but I would generally say that 3 and 4 are tags for games you should not play on
+					// regular livestreams. 1 is hit or miss
+					if num, ok := v.(float64); ok && (num == 3 || num == 4){
+						fmt.Printf("This game likely has sexual content and shouldn't be included: %s\n", game.Name)
+						tagGameAsNSFW(game.AppID, true)
+					}
 				}
 			}
+		} else {
+			// Nothing here
+			fmt.Printf("Game already in list and checkAllGames not set/true. Ignoring\n")
 		}
 	}	
 }
